b2243c79f2b25939a1d56e6544a06dc93ba8ffc5
Garvit <122175234+garvit-exe@users.noreply.github.com>
2023-12-30 17:23:23 +0530

    Create edit.py

---
+++ edit.py
@@ -0,0 +1,23 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = './images'
+
+if not os.path.exists("editedImages"):
+    os.mkdir("editedImages")
+
+pathOut = './editedImages'
+
+for filename in os.listdir(path):
+    img = Image.open(f'{path}/{filename}')
+
+    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(-90)
+    edit = edit.filter(ImageFilter.DETAIL)
+
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
+
+    clean_name = os.path.splitext(filename)[0]
+
+    edit.save(f'{pathOut}/{clean_name}_edited.jpg')
